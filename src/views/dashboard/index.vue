7b03cf15b7ed34f16982c10bd2cdeb597c95fb66
difiry <yanjunye@126.com>
2020-09-04 18:05:57 +0800

    20200904 yyj

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">欢迎您 {{ name }} ！</div>
+    
   </div>
 </template>
 
 <script>
+import { getList } from '@/api/table'
 import { mapGetters } from 'vuex'
 
 export default {
@@ -13,6 +15,34 @@ export default {
     ...mapGetters([
       'name'
     ])
+  },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'gray',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    }
+  },
+  data() {
+    return {
+      list: null,
+      listLoading: true
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    fetchData() {
+      this.listLoading = true
+      getList().then(response => {
+        this.list = response.data.items
+        this.listLoading = false
+      })
+    }
   }
 }
 </script>
