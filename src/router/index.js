6fc149321a2096b016cf83f1e3c662251a1d7e30
difiry <yanjunye@126.com>
2020-09-22 21:17:28 +0800

    20200922 叶炎军

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ Vue.use(Router)
 
 /* Layout */
 import Layout from '@/layout'
-
+import tableRouter from './modules/auditManager'
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -59,52 +59,7 @@ export const constantRoutes = [
       meta: { title: '首页', icon: 'dashboard' }
     }]
   },
-
-  {
-    path: '/auditManager',
-    component: Layout,
-    redirect: '/auditManager/form',
-    name: 'AuditManager',
-    meta: { title: '审计管理', icon: 'el-icon-s-help' },
-    children: [
-      {
-        path: 'form',
-        name: 'Form',
-        component: () => import('@/views/form/index'),
-        meta: { title: '审计立项', icon: 'form' }
-      },
-      {
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
-        meta: { title: '审计抽样', icon: 'table' }
-      },
-      {
-        path: 'auditConfirm',
-        name: 'AuditConfirm',
-        component: () => import('@/views/auditConfirm/index'),
-        meta: { title: '审计确认', icon: 'form' }
-      },
-      {
-        path: 'form2',
-        name: 'Form2',
-        component: () => import('@/views/form/index'),
-        meta: { title: '审计报告征求意见', icon: 'form' }
-      },
-      {
-        path: 'form3',
-        name: 'Form3',
-        component: () => import('@/views/form/index'),
-        meta: { title: '审计报告', icon: 'form' }
-      },
-      {
-        path: 'form4',
-        name: 'Form4',
-        component: () => import('@/views/form/index'),
-        meta: { title: '后续审计', icon: 'form' }
-      }
-    ]
-  },
+  tableRouter,
   {
     path: '/example2',
     component: Layout,
@@ -234,7 +189,6 @@ export const constantRoutes = [
       }
     ]
   },
-  
   {
     path: '/example',
     component: Layout,
--- src/router/modules/auditManager.js
+++ src/router/modules/auditManager.js
@@ -5,19 +5,45 @@ import Layout from '@/layout'
 const tableRouter = {
   path: '/auditManager',
   component: Layout,
-  redirect: '/auditManager/auditConfirm',
-  name: 'auditManager',
-  meta: {
-    title: 'Table',
-    icon: 'table'
-  },
+  redirect: '/auditManager/form',
+  name: 'AuditManager',
+  meta: { title: '审计管理', icon: 'el-icon-s-help' },
   children: [
-    
     {
-      path: 'auditManager',
+      path: 'form',
+      name: 'Form',
+      component: () => import('@/views/form/index'),
+      meta: { title: '审计立项', icon: 'form' }
+    },
+    {
+      path: 'table',
+      name: 'Table',
+      component: () => import('@/views/table/index'),
+      meta: { title: '审计抽样', icon: 'table' }
+    },
+    {
+      path: 'auditConfirm',
+      name: 'AuditConfirm',
       component: () => import('@/views/auditConfirm/index'),
-      name: 'ComplexTable',
-      meta: { title: 'complexTable' }
+      meta: { title: '审计确认', icon: 'form' }
+    },
+    {
+      path: 'form2',
+      name: 'Form2',
+      component: () => import('@/views/form/index'),
+      meta: { title: '审计报告征求意见', icon: 'form' }
+    },
+    {
+      path: 'form3',
+      name: 'Form3',
+      component: () => import('@/views/form/index'),
+      meta: { title: '审计报告', icon: 'form' }
+    },
+    {
+      path: 'form4',
+      name: 'Form4',
+      component: () => import('@/views/form/index'),
+      meta: { title: '后续审计', icon: 'form' }
     }
   ]
 }
